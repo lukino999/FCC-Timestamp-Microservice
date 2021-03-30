240616234b903d8d4e24cb24d8cddd89cd019999
Luca Maccioni <maccion@amazon.com>
2021-03-30 16:42:45 +0100

    add /.well-known/pki-validation endpoint

--- src/server.js
+++ src/server.js
@@ -2,68 +2,68 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 app.use((req, res, next) => {
-  console.log(`${new Date()}:\n${req.originalUrl}, ${req.body}`);
-  next();
-})
+    console.log(`${new Date()}:\n${req.originalUrl}, ${req.body}`);
+    next();
+});
+
+//
+app.use("/.well-known/pki-validation", express.static("src/pki-validation/"));
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({ greeting: 'hello API' });
+    res.json({ greeting: "hello API" });
 });
 
 //
 const jsonFromDate = date => {
-  return { unix: date.getTime(), utc: date.toUTCString() }
-}
+    return { unix: date.getTime(), utc: date.toUTCString() };
+};
 
 // get present time
-app.get('/api/timestamp', (req, res) => {
-  date = new Date();
-  res.json(jsonFromDate(date))
-})
+app.get("/api/timestamp", (req, res) => {
+    date = new Date();
+    res.json(jsonFromDate(date));
+});
 
 // get time from :date_string
-app.get('/api/timestamp/:date_string', (req, res) => {
-  const dateString = req.params.date_string;
-  const time = Date.parse(dateString);
+app.get("/api/timestamp/:date_string", (req, res) => {
+    const dateString = req.params.date_string;
+    const time = Date.parse(dateString);
 
-  if (!isNaN(time)) {
-    // valid ISO string
-    res.json(jsonFromDate(new Date(time)));
-  } else {
-    // time is NaN: check if integer
-    const dateParsed = parseFloat(dateString);
-    if (Number.isInteger(dateParsed)) {
-      res.json(jsonFromDate(new Date(dateParsed)));
+    if (!isNaN(time)) {
+        // valid ISO string
+        res.json(jsonFromDate(new Date(time)));
     } else {
-      // not an integer
-      res.json({ error: "Invalid Date" });
+        // time is NaN: check if integer
+        const dateParsed = parseFloat(dateString);
+        if (Number.isInteger(dateParsed)) {
+            res.json(jsonFromDate(new Date(dateParsed)));
+        } else {
+            // not an integer
+            res.json({ error: "Invalid Date" });
+        }
     }
-  }
-
-})
-
+});
 
 // listen for requests :)
 const PORT = process.env.PORT || 3000;
 var listener = app.listen(PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
